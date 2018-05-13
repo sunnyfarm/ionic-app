e0e9158fc6f9e2fd0e65863b86a1e103cda1b0d7
Huamin Chen <hchen@yummy.home>
2018-05-13 19:21:38 -0400

    update contact page
    Signed-off-by: Huamin Chen <hchen@yummy.home>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,20 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { LinkedIn } from '@ionic-native/linkedin';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    LoginPage,
     HomePage,
     TabsPage
   ],
@@ -27,14 +29,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    LoginPage,
     HomePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LinkedIn
   ]
 })
 export class AppModule {}
